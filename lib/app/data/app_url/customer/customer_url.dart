848e288e9f592d9d24d917e46e5f9197353d235d
Huda <fathimahuda220@gmail.com>
2024-10-15 17:32:31 +0530

    customer

---
+++ lib/app/data/app_url/customer/customer_url.dart
@@ -0,0 +1,9 @@
+import 'package:amina_enterprises_flutter_web/app/constants/strings.dart';
+
+class AppCusUrl {
+  static String baseUrl = kBaseUrl;
+  static String view = '$baseUrl/employee/Getemployee';
+  static String add = '$baseUrl/Customer/add_customer';
+  static String edit = '$baseUrl/Customer/Edit_customer';
+  static String delete = '$baseUrl/Customer/Delete_customer';
+}
