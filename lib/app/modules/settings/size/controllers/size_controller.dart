29695162544a7b030b5e804f949570f46d094faa
anubiju22 <anuit16@gmail.com>
2024-10-21 15:42:42 +0530

    main

--- lib/app/modules/settings/size/controllers/size_controller.dart
+++ lib/app/modules/settings/size/controllers/size_controller.dart
@@ -1,23 +1,42 @@
+import 'package:amina_enterprises_flutter_web/app/data/model/settings/size/size_model.dart';
+import 'package:amina_enterprises_flutter_web/app/domain/entity/status.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SizeController extends GetxController {
-  //TODO: Implement SizeController
+  final rxRequestStatus = Status.completed.obs;
+  RxString error = ''.obs;
+  final formkey = GlobalKey<FormState>();
+ // final _repo = DistrictRepository();
+  TextEditingController nameController = TextEditingController();
+  RxList<SizeData> data = <SizeData>[].obs;
+  String editId = '';
 
-  final count = 0.obs;
   @override
   void onInit() {
+ //   get();
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  void setRxRequestStatus(Status value) => rxRequestStatus.value = value;
+  void setError(String value) => error.value = value;
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+  // void get() async {
+  //   setRxRequestStatus(Status.loading);
+  //   data.clear();
+  //   final res = await _repo.getList();
+  //   res.fold((failure) {
+  //     setRxRequestStatus(Status.completed);
+  //     setError(error.toString());
+  //   }, (resData) {
+  //     setRxRequestStatus(Status.completed);
+  //     if (resData.data != null) {
+  //       data.addAll(resData.data!);
+  //     }
+  //   });
+  // }
 
-  void increment() => count.value++;
+  void delete(String string) {}
+
+  void editClick(SizeData item) {}
 }
